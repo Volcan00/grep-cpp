2c844eff366f950e9f99bd06422c5a88b2718197
dany <dyagnichenko@gmail.com>
2024-09-26 16:09:40 +0200

    Add pattern matching functionality for combined character classes.

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,78 +1,122 @@
 #include <iostream>
 #include <string>
 
-bool matchChar(char pattern, char input) {
-    switch (pattern) {
-        case 'd': return isdigit(input);
-        case 'w': return isalnum(input);
-        default: return pattern == input;
+bool match_digit(const std::string& input_line) {
+    for(size_t i = 0; i < input_line.size(); ++i) {
+        if(isdigit(input_line[i]))
+            return true;
     }
+
+    return false;
+}
+
+bool match_alphanumeric(const std::string input_line) {
+    for(size_t i = 0; i < input_line.size(); ++i) {
+        if(isalnum(input_line[i]))
+            return true;
+    }
+
+    return false;
+}
+
+bool match_positive_group(const std::string& input_line, const std::string& pattern) {
+    std::string char_group = pattern.substr(1, pattern.size() - 2);
+
+    for(char c : char_group) {
+        if(input_line.find(c) != std::string::npos)
+            return true;
+    }
+
+    return false;
+}
+
+bool match_negative_group(const std::string& input_string, const std::string& pattern) {
+    std::string char_group = pattern.substr(2, pattern.size() - 3);
+
+    for(char c : char_group) {
+        if(input_string.find(c) != std::string::npos)
+            return false;
+    }
+
+    return true;
+}
+
+bool match_combined_character_class(const std::string& input_line, const std::string& pattern) {
+    size_t input_pos = 0;
+
+    while(input_pos < input_line.size()) {
+        size_t pattern_pos = 0;
+        size_t input_pos_cpy = input_pos;
+
+        while(input_pos_cpy < input_line.size() && pattern_pos < pattern.size()) {
+            if(pattern[pattern_pos] == '\\') {
+                ++pattern_pos;
+
+                if(pattern_pos < pattern.size()) {
+                    if(pattern[pattern_pos] == 'd') {
+                        if(!isdigit(input_line[input_pos_cpy])) {
+                            break;
+                        }
+                    }
+                    else {
+                        ++input_pos_cpy;
+                    }
+                }
+                else if(pattern[pattern_pos] == 'w') {
+                    if(!isalnum(input_line[input_pos_cpy])) {
+                        break;
+                    }
+                    else {
+                        ++input_pos_cpy;
+                    }
+                }
+                else
+                    break;
+            }
+            else {
+                if(input_line[input_pos_cpy] != pattern[pattern_pos]) {
+                    break;
+                }
+                else {
+                    ++input_pos_cpy;
+                }
+            }
+            ++pattern_pos;
+        }
+        if(pattern_pos == pattern.size()) {
+            return true;
+        }
+        ++input_pos;
+    }
+
+    return false;
 }
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if(!pattern.empty()) {
-        size_t input_pos = 0;
-
-        while(input_pos < input_line.size()) {
-            size_t pattern_pos = 0;
-            size_t temp = input_pos;
-
-            while(pattern_pos < pattern.size() && temp < input_line.size()) {
-                if(pattern[pattern_pos] == '\\') {
-                    ++pattern_pos;
-
-                    if(pattern_pos < pattern.size()) {
-                        if(!matchChar(pattern[pattern_pos], input_line[input_pos])) {
-                            break;
-                        }
-
-                        ++temp;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    if(!matchChar(pattern[pattern_pos], input_line[input_pos])) {
-                        break;
-                    }
-                    else {
-                        ++temp;
-                    }
-                }
-
-                ++pattern_pos;
-            }
-
-            if(pattern_pos == pattern.size()) return true;
-            ++input_pos;
+    if(pattern.size() == 1) {
+        return input_line.find(pattern) != std::string::npos;
+    }
+    else if(pattern[0] == '\\' && pattern.size() == 2) {
+        switch (pattern[1])
+        {
+        case 'd':
+            return match_digit(input_line);
+            break;
+        case 'w':
+            return match_alphanumeric(input_line);
+            break;
+        default:
+            throw std::runtime_error("Unhandled pattern " + pattern);
+            break;
         }
-        
-        return false;
     }
     else if(pattern.front() =='[' && pattern.back() == ']') {
         bool is_negative_group = (pattern[1] == '^');
 
-        std::string char_group = is_negative_group
-                                    ? pattern.substr(2, pattern.length() - 3)
-                                    : pattern.substr(1, pattern.length() - 2);
-
-        if(is_negative_group) {
-            for(char c : char_group) {
-                if(input_line.find(c) != std::string::npos)
-                    return false;
-            }
-
-            return true;
-        }
-        else {
-            for(char c : char_group) {
-                if(input_line.find(c) != std::string::npos)
-                    return true;
-            }
-
-            return false;
-        }
+        return is_negative_group ? match_negative_group(input_line, pattern) : match_positive_group(input_line, pattern);
+    }
+    else if(pattern.length() > 2){
+        return match_combined_character_class(input_line, pattern);
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
