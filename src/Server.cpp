3367c1df31717ea6a1e65049a0608422009ffc26
dany <dyagnichenko@gmail.com>
2024-09-26 12:57:56 +0200

    Add pattern matching functionality for combined character classes.

--- src/Server.cpp
+++ src/Server.cpp
@@ -17,35 +17,70 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
 
     //     return true;
     // }
-    if(!pattern.empty()) {
-        size_t pattern_pos = 0;
-        size_t input_pos =0;
+    // if(!pattern.empty()) {
+    //     size_t pattern_pos = 0;
+    //     size_t input_pos = 0;
 
-        while(pattern_pos < pattern.size() && input_pos < input_line.size()) {
-            if(pattern[pattern_pos] == '\\') {
-                if(pattern_pos + 1 < pattern.size()) {
-                    char special_character = pattern[pattern_pos + 1];
+    //     while(pattern_pos < pattern.size() && input_pos < input_line.size()) {
+    //         if(pattern[pattern_pos] == '\\') {
+    //             if(pattern_pos + 1 < pattern.size()) {
+    //                 char special_character = pattern[pattern_pos + 1];
+
+    //                 if(special_character == 'd') {
+    //                     if(!isdigit(input_line[input_pos]))
+    //                         return false;
+    //                 }
+
+    //                 ++input_pos;
+    //                 pattern_pos += 2;
+
+    //             }
+    //         }
+    //         else {
+    //             if(pattern[pattern_pos != input_line[input_pos]])
+    //                 return false;
+
+    //             ++input_pos;
+    //             ++pattern_pos;
+    //         }
+    //     }
+
+    //     return (pattern_pos == pattern.size());
+    // }
+    if(!pattern.empty()) {
+        size_t patter_pos = 0;
+        size_t input_pos = 0;
+
+        while(patter_pos < pattern.length() && input_pos < input_line.length()) {
+            if(pattern[patter_pos] == '\\') {
+                if(patter_pos + 1 < pattern.length()) {
+                    char special_character = pattern[patter_pos + 1];
 
                     if(special_character == 'd') {
-                        if(!isdigit(input_line[input_pos]))
+                        while(input_pos < input_line.length() && !isdigit(input_line[input_pos])) {
+                            ++input_pos;
+                        }
+
+                        if(input_pos >= input_line.length()) {
                             return false;
+                        }
+                    }
+                    else if(special_character == 'w') {
+                        return !isalnum(input_line[input_pos]);
                     }
 
                     ++input_pos;
-                    pattern_pos += 2;
-
+                    patter_pos += 2;
                 }
             }
-            else {
-                if(pattern[pattern_pos != input_line[input_pos]])
+            else if(isalpha(pattern[patter_pos])) {
+                if(pattern[patter_pos] != input_line[input_pos]) {
                     return false;
-
-                ++input_pos;
-                ++pattern_pos;
+                }
             }
         }
 
-        return (pattern_pos == pattern.size());
+        return true;
     }
     else if(pattern.front() =='[' && pattern.back() == ']') {
         bool is_negative_group = (pattern[1] == '^');
